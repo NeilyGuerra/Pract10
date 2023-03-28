494518d1720c561616c8cc7e9d8150b2b8e7422f
Sergioac52 <sergio.antona1@educa.madrid.org>
2023-03-28 10:27:34 +0200

    Mover esPrimo a la clase Numeros

--- src/main/java/ieseuropa/Numeros.java
+++ src/main/java/ieseuropa/Numeros.java
@@ -35,12 +35,21 @@ public class Numeros {
 		if(suma == numero)return true;
 		else return false;
 	}
-
+	private static boolean esPrimo (int num) {
+		for(int i=2 ;i<num;i++) {
+			if(num%i==0) 
+				return false ;	
+		}
+		return true;
+	}
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(calcularSeriePell()));
 		int num = 153;
 		if(esAmstrong(num)) System.out.println("El número " + num + " es un número Amstrong.");
 		else System.out.println("El número " + num + " no es un número Amstrong.");
+		
+		int primo = 5;
+		System.out.println("es "+primo+" primo: "+esPrimo(primo));
 	}
 
 }

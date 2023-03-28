169c35b7fc0007c4a8043dd2c2137ef6aa3a91af
TilinExe816 <127988580+TilinExe816@users.noreply.github.com>
2023-03-28 16:22:00 +0200

    Update Array.java

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -14,9 +14,30 @@ public class Array {
 	    return minimo;
 	}
 
+	public static double calcularMediana(double[] numeros) {
+	    Arrays.sort(numeros);
+	    int mitad = numeros.length / 2;
+	    if (numeros.length % 2 == 0) {
+	        return (numeros[mitad - 1] + numeros[mitad]) / 2;
+	    } else {
+	        return numeros[mitad];
+	    }
+	}
+	
+	public static double calcularMedia(double[] numeros) {
+	    double suma = 0;
+	    for (int i = 0; i < numeros.length; i++) {
+	        suma += numeros[i];
+	    }
+	    return suma / numeros.length;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		double[]numeros = {1,2,3,4,5,6,7,8,9};
+		System.out.println(calcularMediana(numeros);
+		System.out.println(calcularMedia(numeros);
+		System.out.println(calcularMinimo(numeros);
 	}
 
 }

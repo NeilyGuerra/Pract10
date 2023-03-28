503d12358015428690537e59b0c2236f8a98ad10
TilinExe816 <nuevaunion22@gmail.com>
2023-03-28 15:59:11 +0200

    Media

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,6 +1,15 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	public static double calcularMedia(double[] numeros) {
+	    double suma = 0;
+	    for (int i = 0; i < numeros.length; i++) {
+	        suma += numeros[i];
+	    }
+	    return suma / numeros.length;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

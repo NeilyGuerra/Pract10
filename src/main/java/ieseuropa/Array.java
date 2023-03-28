c07037f33379bd84703ee2f2bd069f7f3601489b
TilinExe816 <nuevaunion22@gmail.com>
2023-03-28 16:33:01 +0200

    Ordenar

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,18 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+
 public class Array {
+	
+	public static void ordenarArray(double[] numeros) {
+	    Arrays.sort(numeros);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		double[] numeros = {7.8, 5.6, 3.4, 1.2};
+		ordenarArray(numeros);
+		System.out.println("El array ordenado es: " + Arrays.toString(numeros));
 	}
 
 }
